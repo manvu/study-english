cfc4db9aa79f8efa7bf6ec7f7e0a3d2fb1b73e4c
Man Vu <manvminh@gmail.com>
2021-06-08 14:52:17 -0400

    test

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,8 @@
 const mysql = require("mysql");
-const {
-  mysql_host,
-  mysql_user,
-  mysql_port,
-  mysql_password,
-  database_name,
-} = require("./index");
+const { mysql_host, mysql_user, mysql_port, mysql_password, database_name } = require("./index");
 
 /**
- * Database configuration that is consumed by models
+ * Database configuration that is consumed by models 
  */
 class Database {
   /**
@@ -16,32 +10,24 @@ class Database {
    */
   constructor() {
     this.pool = mysql.createPool({
-      connectionLimit: 30,
+      connectionLimit: 10,
       host: mysql_host,
       user: mysql_user,
       port: mysql_port,
       password: mysql_password,
       database: database_name,
       debug: false,
-      multipleStatements: true,
+      multipleStatements: true 
     });
 
     /**
      * Function executes query passed in by model
-     * Release a connection when a query is done
      * @param {*} query Query passed in by model
      */
     this.executeQuery = async function(query) {
       return new Promise((resolve) =>
-        this.pool.getConnection((err, conn) => {
-          if (err) {
-            resolve({ error: err, response: null });
-          } else {
-            this.pool.query(query, function(err, response, fields) {
-              conn.destroy()
-              resolve({ error: err, response: response });
-            });
-          }
+        this.pool.query(query, function(err, response, fields) {
+          resolve({ error: err, response: response });
         })
       );
     };
